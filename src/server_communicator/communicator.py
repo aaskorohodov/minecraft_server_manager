0ef57056e2fb289668cd9c7325bb9f9c285c1627
DY <aaskorohodov@gmail.com>
2026-02-16 17:28:12 +0500

    debug

--- src/server_communicator/communicator.py
+++ src/server_communicator/communicator.py
@@ -1,5 +1,6 @@
 import threading
 import subprocess
+import time
 
 from loguru import logger
 from queue import Queue, Empty
@@ -32,6 +33,21 @@ class ServerCommunicator:
 
         # Thread 2: Consumer (Processes data)
         threading.Thread(target=self._processor_loop, daemon=True).start()
+
+        threading.Thread(target=self._ping, daemon=True).start()
+
+    def _ping(self) -> None:
+        """"""
+
+        while not self._stop_event.is_set() or not self._output_queue.empty():
+            try:
+                player_name = 'NonExistingPlayer123'
+                message = 'Some message'
+                command = f"tellraw {player_name} {message}\n"
+                self.send_to_server(command)
+                time.sleep(30)
+            except Exception as e:
+                logger.exception(e)
 
     def _reader_loop(self) -> None:
         """"""
